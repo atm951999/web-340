01bef894208c384ba14af6d22ed2fc3ef3ba5941
Angela Martin <amartin3@woh.rr.com>
2021-05-02 21:59:34 -0400

    update 7

--- ems/models/employee.js
+++ ems/models/employee.js
@@ -1,19 +1,28 @@
+/*
+============================================
+; Title: Assignment 7.4
+; Author: Professor Cross
+; Date: 02 May 2021
+; Modified By: Angela Martin
+; Description: This program demonstrates the 
+; use of Node.js.
+;===========================================
+*/
+
+// Create mongoose variable
 var mongoose = require("mongoose");
 
+// Create Schema variable
 var Schema = mongoose.Schema;
 
-// define the employeeSchema
-
+// Create employeeSchema variable
 var employeeSchema = new Schema({
     firstName: { type: String, required: true, unique: true},
     lastName: { type: String, required: true, unique: true},
 
 });
 
-// define the employee model
-
+// Create employee variable
 var Employee = mongoose.model("Employee", employeeSchema);
-
-// expose the employee to calling files
 
 module.exports = Employee;
